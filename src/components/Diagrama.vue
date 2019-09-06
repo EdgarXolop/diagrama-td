c56030816ff486eba4588e0d6ee2c627f843086f
Edgar Xolop <edgarxoix@gmail.com>
2019-09-06 00:42:52 -0600

    refactor code

--- src/components/Diagrama.vue
+++ src/components/Diagrama.vue
@@ -106,7 +106,8 @@ export default {
         links: [],
         match: false,
         result_message: '',
-        test: ''
+        test: '',
+        arc: true
     }),
     methods: {
         generate() {
@@ -119,6 +120,116 @@ export default {
                 links: this.links
             }
 
+            if(this.arc) generateArcDiagram(data)
+            else generateNetworkDiagram(data)
+
+        },
+        generateLinks() {
+            let links = [];
+            let row;
+
+            for (let i = 0; i < this.values.length; i++) {
+
+                row = this.values[i]
+
+                for (let q = 0; q < row.length; q++) {
+
+                    if (row[q] > 1) {
+
+                        links.push({
+                            source: i + 1,
+                            target: row[q] - 1,
+                        })
+
+                    }
+
+                }
+            }
+
+            this.links = links
+            
+            /*.reduce((acc, current) => {
+                const x = acc.find(item => item.target === current.target && item.source === current.source);
+                if (!x) {
+                    return acc.concat([current]);
+                } else {
+                    return acc;
+                }
+            }, [])*/
+        },
+        generateNodes() {
+            let nodes = [];
+            let value;
+
+            for (let r = 0; r < this.rows.length; r++) {
+
+                value = this.rows[r];
+                if (this.liniksHasNode(value))
+                    nodes.push({
+                        id: value,
+                        name: `Q${value}`,
+                        accept: this.values[r][5] == 0
+                    });
+
+            }
+
+            this.nodes = nodes;
+        },
+        liniksHasNode(node) {
+
+            for (let i in this.links) {
+
+                if (this.links[i].target == node || this.links[i].source == node) return true
+
+            }
+
+            return false
+        },
+        generateData() {
+            this.generateLinks()
+            this.generateNodes()
+        },
+        testText(){
+            
+            let correct_info = this.values.length > 0
+
+
+            if(correct_info){
+
+                this.result_message = ""
+                let charts = this.test.split("")
+                let row,rule_index,row_index=0
+
+                
+                for(let i in charts){
+                    row = this.values[row_index]
+
+                    rule_index = this.rules.indexOf(charts[i])
+
+                    if( rule_index != -1){
+                        
+                        this.match = row[rule_index] == 0 || (charts.length -1 == i && row[5] == 0)
+
+                        if( row[rule_index] - 2 > 0 ) row_index = row[rule_index] - 2
+                        
+                    }else{
+                        
+                        this.match=false
+                    }
+                    
+                }
+
+                return;
+            }
+            
+            this.match = false
+
+            this.result_message = "Parametros incompletos"
+        }
+    },
+}
+
+function generateArcDiagram(data){
 
             // set the dimensions and margins of the graph
             var
@@ -274,111 +385,95 @@ export default {
                         .style("stroke-opacity", 1)
 
                 })
+}
 
+function generateNetworkDiagram(data){
+    
 
-        },
-        generateLinks() {
-            let links = [];
-            let row;
+            // set the dimensions and margins of the graph
+            var margin = {top: 10, right: 30, bottom: 30, left: 40},
+            width = document.getElementById("graph").clientWidth - margin.left - margin.right,
+            height = 600 - margin.top - margin.bottom;
 
-            for (let i = 0; i < this.values.length; i++) {
+            // append the svg object to the body of the page
+            var svg = d3.select("#graph")
+            .append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform",
+                    "translate(" + margin.left + "," + margin.top + ")");
 
-                row = this.values[i]
+            //defines arrow
+            svg.append("svg:defs")
+                .append("svg:marker")
+                .attr("id", "arrow")
+                .attr("viewBox", "0 0 12 12")
+                .attr("refX", 3)
+                .attr("refY", 3)
+                .attr("markerWidth", 12)
+                .attr("markerHeight", 12)
+                .attr("orient", "auto")
+                .append("path")
+                .attr("d", "M1,1 L5,3 L1,5 L3,3 L1,1")
+                .style("fill", "#000")
 
-                for (let q = 0; q < row.length; q++) {
+            // Initialize the links
+            var link = svg
+                .selectAll("line")
+                .data(data.links)
+                .enter()
+                .append("line")
+                .style("stroke", function(d,i){return colorArray[i]})
+                .style("stroke-width", "3")
+                .attr("marker-start", "url(#arrow)")
+                .attr("marker-mid", "url(#arrow)")
+                .attr("marker-end", "url(#arrow)");
 
-                    if (row[q] > 1) {
-
-                        links.push({
-                            source: i + 1,
-                            target: row[q] - 1,
-                        })
-
-                    }
-
-                }
-            }
-
-            this.links = links
-            
-            /*.reduce((acc, current) => {
-                const x = acc.find(item => item.target === current.target && item.source === current.source);
-                if (!x) {
-                    return acc.concat([current]);
-                } else {
-                    return acc;
-                }
-            }, [])*/
-        },
-        generateNodes() {
-            let nodes = [];
-            let value;
-
-            for (let r = 0; r < this.rows.length; r++) {
-
-                value = this.rows[r];
-                if (this.liniksHasNode(value))
-                    nodes.push({
-                        id: value,
-                        name: `Q${value}`,
-                        accept: this.values[r][5] == 0
-                    });
-
-            }
-
-            this.nodes = nodes;
-        },
-        liniksHasNode(node) {
-
-            for (let i in this.links) {
-
-                if (this.links[i].target == node || this.links[i].source == node) return true
-
-            }
-
-            return false
-        },
-        generateData() {
-            this.generateLinks()
-            this.generateNodes()
-        },
-        testText(){
-            
-            let correct_info = this.values.length > 0
-
-
-            if(correct_info){
-
-                this.result_message = ""
-                let charts = this.test.split("")
-                let row,rule_index,row_index=0
-
+            // Initialize the nodes
+            var node = svg
+                .selectAll("g")
+                .data(data.nodes)
+                .enter()
+                .append("g")
                 
-                for(let i in charts){
-                    row = this.values[row_index]
+            node 
+                .append("circle")
+                .attr("r", 20)
+                .style("fill", "#005cb2")
+                .style("stroke", function(d){ return d.accept ? "6ab7ff" : null })
+                .style("stroke-width", function(d){ return d.accept ? 5 : 0 })
+            node
+                .append("text")
+                .attr("dx", function(){return -10})
+                .attr("dy", function(){return 5})
+                .style('fill', '#fff')
+                .text(function(d){return d.name})
 
-                    rule_index = this.rules.indexOf(charts[i])
+            // Let's list the force we wanna apply on the network
+            d3.forceSimulation(data.nodes)                 // Force algorithm is applied to data.nodes
+                .force("link", d3.forceLink()                               // This force provides links between nodes
+                        .id(function(d) { return d.id; })                     // This provide  the id of a node
+                        .links(data.links)                                    // and this the list of links
+                        .distance(100)
+                        .strength(0.1)
+                )
+                .force("charge", d3.forceManyBody().strength(-800))         // This adds repulsion between nodes. Play with the -400 for the repulsion strength
+                .force("center", d3.forceCenter(width / 2, height / 2))     // This force attracts nodes to the center of the svg area
+                .force("yAxis",d3.forceY(height/2))
+                .on("end", ticked);
 
-                    if( rule_index != -1){
-                        
-                        this.match = row[rule_index] == 0 || (charts.length -1 == i && row[5] == 0)
+            // This function is run at each iteration of the force algorithm, updating the nodes position.
+            function ticked() {
+                link
+                    .attr("x1", function(d) { return d.source.x; })
+                    .attr("y1", function(d) { return d.source.y; })
+                    .attr("x2", function(d) { return d.target.x; })
+                    .attr("y2", function(d) { return d.target.y; });
 
-                        if( row[rule_index] - 2 > 0 ) row_index = row[rule_index] - 2
-                        
-                    }else{
-                        
-                        this.match=false
-                    }
-                    
-                }
-
-                return;
+                node
+                    .attr("transform", function (d) { return `translate(${d.x+6},${d.y-6})` })
             }
-            
-            this.match = false
 
-            this.result_message = "Parametros incompletos"
-        }
-    },
 }
 </script>
